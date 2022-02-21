df32add367bd358aeb8c6fd60133c46e7429a035
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2022-02-21 22:30:16 +0100

    endMail

---
+++ routes/api/usersСurrent.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const createError = require("http-errors");
+const Joi = require("joi");
+const { authenticate, upload } = require("../../middlewares");
+const { User, schemas } = require("../../models/user");
+const { sendMail } = require("../../helpers");
+const router = express.Router();
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw createError(404);
+    }
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: "",
+    });
+    res.json({
+      message: "Verification successful",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { error } = schemas.verify.validate(req.body);
+    if (error) {
+      throw createError(400, "missing required field email");
+    }
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (user.verify) {
+      throw createError(400, "Verification has already been passed");
+    }
+    const mail = {
+      to: email,
+      subject: "Подтвеждение email",
+      html: `<a target="_blank" href='http://localhost:3000/api/users/${user.verificationToken}'>Нажмите чтобы подтвердить свой email</a>`,
+    };
+    sendMail(mail);
+    res.json({
+      message: "Verification email sent",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/current", authenticate, async (req, res, next) => {
+  res.json({
+    email: req.user.email,
+  });
+});
+
+router.get("/logout", authenticate, async (req, res, next) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).send();
+});
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  async (req, res, next) => {
+    const { _id } = req.user;
+    const { path: tempUpload, filename } = req.file;
+    try {
+      const [extention] = filename.split(".").reverse();
+      const newFileName = `${_id}.${extention}`;
+      const resultUpload = path.join(avatarsDir, newFileName);
+      await fs.rename(tempUpload, resultUpload);
+      const avatarURL = path.join("avatars", newFileName);
+      await User.findByIdAndUpdate(_id, { avatarURL });
+      res.json({
+        avatarURL,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Vasyl:171717vaska171717@cluster0.jsto4.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(4000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
